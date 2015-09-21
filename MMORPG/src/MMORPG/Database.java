13d7e0582a98c6eda71ea4b005f930ea7100844a
KevinPC <keviee007@live.nl>
2015-09-21 22:05:42 +0200

    0.2.6

--- MMORPG/src/MMORPG/Database.java
+++ MMORPG/src/MMORPG/Database.java
@@ -9,8 +9,10 @@ import databaseEntity.User;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.Query;
 
 /**
@@ -18,10 +20,11 @@ import javax.persistence.Query;
  * @author Lars
  */
 public class Database {
-     @PersistenceContext
+     @PersistenceContext(unitName = "MMORPGPU", type = PersistenceContextType.EXTENDED)
      private static EntityManager em;
     public Database(){
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MMORPGPU");
+        em = emf.createEntityManager();
     }
     
     public User findUser(String username) {
@@ -31,12 +34,28 @@ public class Database {
         query.setParameter("username", username);
         user = (User) query.getSingleResult();
         }catch(Exception e){   
-
         }
+        System.out.println(user);
         return user;
     }
-
-
-   
     
+    public User registerUser(String username, String password){
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);       
+        
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        
+        try{
+        em.persist(user);
+        transaction.commit();
+        em.getTransaction().begin();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+        em.close();
+        }
+        return user;
+    }     
 }
